425ef8c3d5e9c7391ee124a167dd32b6e0e650a9
Amiel Martin <amiel.martin@gmail.com>
2013-01-15 16:45:14 -0800

    Actually redirect

--- lib/app.rb
+++ lib/app.rb
@@ -37,8 +37,8 @@ class App
   end
 
   def redirect(to)
-    # [ 301, {'Location' => to, 'Content-Type' => 'text/plain'}, ["Redirecting to #{to}"]]
-    [200, {'Content-Type' => 'text/plain'}, ["Redirecting to #{to}"]]
+    [ 301, {'Location' => to, 'Content-Type' => 'text/plain'}, ["Redirecting to #{to}"]]
+    # [200, {'Content-Type' => 'text/plain'}, ["Redirecting to #{to}"]]
   end
 
   def get_url_for_pattern
