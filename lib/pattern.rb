5857e4b9733aa94a09e3fe5c75aa5740d141d7f4
Amiel Martin <amiel.martin@gmail.com>
2013-01-15 16:57:37 -0800

    Some shortcuts

--- lib/pattern.rb
+++ lib/pattern.rb
@@ -1,10 +1,20 @@
 require 'active_support/all'
 
 class Pattern
+  NAMES = {
+    'holygrail' => %w[2.5p 3.5p 4.5p],
+    'babydragon' => %w[4.5p 2.5p 1 1 1],
+  }
+
+
   attr_reader :pattern
   def initialize(pattern)
-    # TODO: Test the crap out of this, it looks like black magic
-    @pattern = pattern.split(/\s+|(?<=p)|((?<=\d)(?=\d))/).select(&:present?)
+    if NAMES['pattern']
+      @pattern = NAMES['pattern']
+    else
+      # TODO: Test the crap out of this, it looks like black magic
+      @pattern = pattern.split(/\s+|(?<=p)|((?<=\d)(?=\d))/).select(&:present?)
+    end
   end
 
   def period
