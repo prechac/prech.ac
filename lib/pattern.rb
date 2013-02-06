1c006bd1c8e49b699fd32cfb06e639c1425f53b9
Amiel Martin <amiel.martin@gmail.com>
2013-02-06 14:36:35 -0800

    Support for non-2-person patterns in the easter egg patterns

--- lib/pattern.rb
+++ lib/pattern.rb
@@ -4,17 +4,21 @@ class Pattern
   # Easter egg names.
   # TODO: Add more. Pull-requests accepted.
   NAMES = {
-    'holygrail' => %w[2.5p 3.5p 4.5p],
-    'babydragon' => %w[4.5p 2.5p 1 1 1],
-    'babydragontwins' => %w[4.5p 2.5p 4.5p 2.5p 1],
-    'thedragon' => %w[4.5p 2.5p 2],
+    'holygrail' => [2, %w[2.5p 3.5p 4.5p]],
+    'babydragon' => [2, %w[4.5p 2.5p 1 1 1]],
+    'babydragontwins' => [2, %w[4.5p 2.5p 4.5p 2.5p 1]],
+    'thedragon' => [2, %w[4.5p 2.5p 2]],
+    'yukishomework' => [3, %w[4p 2p 1 3p 3 1]],
+
+    # TODO: Pick a good name for this pattern.
+    'awesometriangle' => [3, %w[3p 4p 3 3 4p 3]],
   }
 
   attr_reader :pattern, :number_of_people
   def initialize(pattern)
     @source = pattern.dup
     if NAMES[pattern]
-      @pattern = NAMES[pattern]
+      @number_of_people, @pattern = NAMES[pattern]
     else
       @number_of_people, @pattern = self.class.parse_pattern(pattern.dup)
     end
